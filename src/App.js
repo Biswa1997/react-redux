717e1ea9196c63bca64ebf72e03a61c28dfc2877
Biswajit <biswajit.sarkar199724@gmail.com>
2022-09-04 02:19:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+      <h1>React-Redux</h1>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
